f9d12067878d57e8edd46b4692eeed4893d2fbc5
Diana Petrosova <petrosova.di@gmail.com>
2023-01-13 17:42:21 -0500

    06 - Customize Initialization Process

--- bin/custom/merchandise/merchandiseinitialdata/src/de/hybris/merchandise/initialdata/setup/InitialDataSystemSetup.java
+++ bin/custom/merchandise/merchandiseinitialdata/src/de/hybris/merchandise/initialdata/setup/InitialDataSystemSetup.java
@@ -6,6 +6,9 @@ package de.hybris.merchandise.initialdata.setup;
 import de.hybris.platform.commerceservices.dataimport.impl.CoreDataImportService;
 import de.hybris.platform.commerceservices.dataimport.impl.SampleDataImportService;
 import de.hybris.platform.commerceservices.setup.AbstractSystemSetup;
+import de.hybris.platform.commerceservices.setup.data.ImportData;
+import de.hybris.platform.commerceservices.setup.events.CoreDataImportedEvent;
+import de.hybris.platform.commerceservices.setup.events.SampleDataImportedEvent;
 import de.hybris.platform.core.initialization.SystemSetup;
 import de.hybris.platform.core.initialization.SystemSetup.Process;
 import de.hybris.platform.core.initialization.SystemSetup.Type;
@@ -15,6 +18,7 @@ import de.hybris.platform.core.initialization.SystemSetupParameterMethod;
 import de.hybris.merchandise.initialdata.constants.MerchandiseInitialDataConstants;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -33,6 +37,7 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 	private static final String IMPORT_CORE_DATA = "importCoreData";
 	private static final String IMPORT_SAMPLE_DATA = "importSampleData";
 	private static final String ACTIVATE_SOLR_CRON_JOBS = "activateSolrCronJobs";
+	private static final String MERCHANDISE = "hybris";
 
 	private CoreDataImportService coreDataImportService;
 	private SampleDataImportService sampleDataImportService;
@@ -94,9 +99,20 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 	@SystemSetup(type = Type.PROJECT, process = Process.ALL)
 	public void createProjectData(final SystemSetupContext context)
 	{
-		/*
-		 * Add import data for each site you have configured
-		 */
+		final List<ImportData> importData = new ArrayList<ImportData>();
+
+		final ImportData hybrisImportData = new ImportData();
+
+		hybrisImportData.setProductCatalogName(MERCHANDISE);
+		hybrisImportData.setContentCatalogNames(Arrays.asList(MERCHANDISE));
+		hybrisImportData.setStoreNames(Arrays.asList(MERCHANDISE));
+		importData.add(hybrisImportData);
+
+		getCoreDataImportService().execute(this, context, importData);
+		getEventService().publishEvent(new CoreDataImportedEvent(context, importData));
+
+		getSampleDataImportService().execute(this, context, importData);
+		getEventService().publishEvent(new SampleDataImportedEvent(context, importData));
 	}
 
 	public CoreDataImportService getCoreDataImportService()
