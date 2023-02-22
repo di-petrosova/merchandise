4cada57d987d4d2779ecee89c9aacd6a897ce707
Diana Petrosova <petrosova.di@gmail.com>
2023-02-22 10:02:09 -0500

    14 - Creating New CMS Components (ant extgen)

---
+++ bin/custom/randomimageaddon/acceleratoraddon/web/src/de/hybris/platform/addons/randomimageaddon/constants/RandomimageaddonWebConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.platform.addons.randomimageaddon.constants;
+
+/**
+ * Global class for all Randomimageaddon web constants. You can add global constants for your extension into this class.
+ */
+public final class RandomimageaddonWebConstants // NOSONAR
+{
+	private RandomimageaddonWebConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
---
+++ bin/custom/randomimageaddon/src/de/hybris/platform/addons/randomimageaddon/constants/RandomimageaddonConstants.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.platform.addons.randomimageaddon.constants;
+
+/**
+ * Global class for all Randomimageaddon constants. You can add global constants for your extension into this class.
+ */
+@SuppressWarnings("deprecation")
+public final class RandomimageaddonConstants extends GeneratedRandomimageaddonConstants
+{
+	public static final String EXTENSIONNAME = "randomimageaddon";
+
+	private RandomimageaddonConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
