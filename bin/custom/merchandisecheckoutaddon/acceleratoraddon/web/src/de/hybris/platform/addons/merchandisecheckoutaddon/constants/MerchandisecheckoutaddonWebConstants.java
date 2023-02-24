74a77eefd45d4ce4c0a9c7c175b41d7223b1647b
Diana Petrosova <petrosova.di@gmail.com>
2023-02-23 21:59:18 -0500

    18 - Customize Checkout - Gift Wrapping (ant extgen)

---
+++ bin/custom/merchandisecheckoutaddon/acceleratoraddon/web/src/de/hybris/platform/addons/merchandisecheckoutaddon/constants/MerchandisecheckoutaddonWebConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.platform.addons.merchandisecheckoutaddon.constants;
+
+/**
+ * Global class for all Merchandisecheckoutaddon web constants. You can add global constants for your extension into this class.
+ */
+public final class MerchandisecheckoutaddonWebConstants // NOSONAR
+{
+	private MerchandisecheckoutaddonWebConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
---
+++ bin/custom/merchandisecheckoutaddon/src/de/hybris/platform/addons/merchandisecheckoutaddon/constants/MerchandisecheckoutaddonConstants.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.platform.addons.merchandisecheckoutaddon.constants;
+
+/**
+ * Global class for all Merchandisecheckoutaddon constants. You can add global constants for your extension into this class.
+ */
+@SuppressWarnings("deprecation")
+public final class MerchandisecheckoutaddonConstants extends GeneratedMerchandisecheckoutaddonConstants
+{
+	public static final String EXTENSIONNAME = "merchandisecheckoutaddon";
+
+	private MerchandisecheckoutaddonConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
