657341840f72409fcd63eec0f2cbac5d185b2168
Diana Petrosova <petrosova.di@gmail.com>
2023-02-28 11:27:45 -0500

    18 - Customize Checkout - Gift Wrapping

--- bin/custom/merchandisecheckoutaddon/acceleratoraddon/web/src/de/hybris/platform/addons/merchandisecheckoutaddon/controllers/MerchandisecheckoutaddonControllerConstants.java
+++ bin/custom/merchandisecheckoutaddon/acceleratoraddon/web/src/de/hybris/platform/addons/merchandisecheckoutaddon/controllers/MerchandisecheckoutaddonControllerConstants.java
@@ -8,4 +8,7 @@ package de.hybris.platform.addons.merchandisecheckoutaddon.controllers;
 public interface MerchandisecheckoutaddonControllerConstants
 {
 	// implement here controller constants used by this extension
+    final String ADDON_PREFIX = "addon:/merchandisecheckoutaddon/";
+    String GiftWrapPage = ADDON_PREFIX + "pages/checkout/multi/giftWrapPage";
+    String ChooseDeliveryMethodPage = ADDON_PREFIX + "pages/checkout/multi/chooseDeliveryMethod";
 }
